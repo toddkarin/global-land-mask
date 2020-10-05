2777eca46cd0d62c8873bd96ddc56b7d9e74770e
toddkarin <toddkarin@lbl.gov>
2020-10-05 06:03:48 -0700

    Change to map of world

--- example_plot_globe_map.py
+++ example_plot_globe_map.py
@@ -1,5 +1,5 @@
 """
-Make map of CONUS
+Example make map of world
 """
 
 from global_land_mask import globe
@@ -11,8 +11,8 @@ import matplotlib.pyplot as plt
 
 
 # Lat/lon points to get
-lat = np.linspace(-20,90,1000)
-lon = np.linspace(-130,-60,1002)
+lat = np.linspace(-90,90,1000)
+lon = np.linspace(-180,180,1002)
 
 # Make a grid
 lon_grid, lat_grid = np.meshgrid(lon,lat)
@@ -25,14 +25,9 @@ fig = plt.figure(0, figsize=(5.5, 4.5))
 plt.clf()
 ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
 
-m = Basemap(llcrnrlon=-119, llcrnrlat=22, urcrnrlon=-64, urcrnrlat=49,
-            projection='lcc', lat_1=33, lat_2=45, lon_0=-95,
-            area_thresh=200,
-            resolution='i')
-m.drawstates(linewidth=0.2)
-m.drawcoastlines(linewidth=0.2)
-m.drawcountries(linewidth=0.2)
-
+m = Basemap(projection='cyl', resolution=None,
+            llcrnrlat=-90, urcrnrlat=90,
+            llcrnrlon=-180, urcrnrlon=180)
 
 cs = m.contourf(lon_grid, lat_grid, z,
                 levels=[-0.5, 0.5,1.5],
@@ -40,7 +35,7 @@ cs = m.contourf(lon_grid, lat_grid, z,
                 latlon=True)
 plt.show()
 
-plt.savefig('example_plot_globe_map_us.png',
+plt.savefig('example_plot_globe_map_globe.png',
             bbox_inches='tight',
             dpi=400)
 
