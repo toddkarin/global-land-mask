17c375b505e0380fb45d36e2d0627d05f96bf7d8
Marius Merschformann <marius.merschformann@gmail.com>
2022-04-13 00:20:35 +0200

    Adding install_requires

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt") as fp:
+    install_requires = fp.read()
+
 setuptools.setup(
     name="global-land-mask",
     version="1.0.0",
@@ -13,6 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/toddkarin/global-land-mask",
     packages=setuptools.find_packages(),
+    install_requires=install_requires,
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
